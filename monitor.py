c71aa8490827677b1b66546ab9074ffa4aeeabe7
Juan Pedro Escalona <jescalona@theworkshop.com>
2018-02-10 15:15:11 +0100

    Revamped the way to get the info and the file config structure

--- monitor.py
+++ monitor.py
@@ -3,93 +3,125 @@ import time
 import requests
 import yaml
 import re
+import sys
+import json
 from influxdb import InfluxDBClient
 from bs4 import BeautifulSoup
 from requests.exceptions import ConnectionError
 from datetime import datetime
 
-def load_servers():
-    with open("/opt/monitor/servers.yaml", 'r') as stream:
+
+MAX_RETRIES = 3
+CURRENT_TIME = datetime.utcnow().strftime('%Y-%m-%dT%H:%M:%SZ')    
+
+
+
+def load_radios(file_path):
+    with open(file_path, 'r') as stream:
         try:
             return yaml.load(stream)
         except yaml.YAMLError as exc:
             print(exc)    
             exit(1)
 
-list_of_servers = load_servers()
-measurements = []
-current_time = datetime.utcnow().strftime('%Y-%m-%dT%H:%M:%SZ')
-
-for server_name, server_info in list_of_servers.iteritems():
-    while True:
-        url_current_users = '{protocol}://{server_host}:{server_port}/{server_path}'.format(
-            protocol='https' if server_info.get('secure') else 'http',
-            server_host=server_info.get('server'),
-            server_port=server_info.get('port'),
-            server_path=server_info.get('path', 'index.html?sid=1')
-        )
-        url_current_song = '{protocol}://{server_host}:{server_port}/{server_path}'.format(
-            protocol='https' if server_info.get('secure') else 'http',
-            server_host=server_info.get('server'),
-            server_port=server_info.get('port'),
-            server_path=server_info.get('current_song', 'currentsong?sid=1')
-        )
-
+def get_radio_show_name(server, retries=MAX_RETRIES):
+    while retries > 0:
         try:
+            url_current_song = '{protocol}://{server_host}:{server_port}/{server_path}'.format(
+                protocol='https' if server.get('secure') else 'http',
+                server_host=server.get('host'),
+                server_port=server.get('port'),
+                server_path=server.get('current_song', 'currentsong?sid=1')
+            )
+            r = requests.get(url_current_song)
+            if r.status_code == requests.codes.ok:
+                return r.content
+        except ConnectionError as e:
+            pass
+        except Exception:
+            pass
+        finally:
+            retries -= 1
+            time.sleep(0.5)
+
+def get_server_stats(server, retries=MAX_RETRIES):
+    while retries > 0:
+        try:
+            url_current_users = '{protocol}://{server_host}:{server_port}/{server_path}'.format(
+                protocol='https' if server.get('secure') else 'http',
+                server_host=server.get('host'),
+                server_port=server.get('port'),
+                server_path=server.get('path', 'index.html?sid=1')
+            )
             r = requests.get(url_current_users)
             if r.status_code == requests.codes.ok:
                 soup = BeautifulSoup(r.content, "html.parser")
                 text = soup.find("td", text="Stream Status: ").find_next_sibling("td").text
-                radio_name = server_info.get('radio_name', server_name)
-                current_song_r = requests.get(url_current_song)
                 shoutcast_info = re.match("Stream is (?P<up>up).* with (?P<users>\d+) of.*", text)
                 if shoutcast_info:
-                    measurements.append({
-                        "measurement": "radio_status",
-                        "tags": {
-                            "radio_name": radio_name,
-                            "origin": server_info.get('origin'),
-                            "server_name": server_name
-                        },
-                        "time": current_time,
-                        "fields": {
-                            "value": 1 if shoutcast_info.group(1) else 0
-                        }
-                    })
-                    measurements.append({
-                        "measurement": "radio_listeners",
-                        "tags": {
-                            "radio_name": radio_name,
-                            "origin": server_info.get('origin'),
-                            "server_name": server_name,
-                            "current_song": current_song_r.content
-                        },
-                        "time": current_time,
-                        "fields": {
-                            "value": int(shoutcast_info.group(2))
-                        }
-                    })
-                    measurements.append({
-                        "measurement": "radio_current_song",
-                        "tags": {
-                            "radio_name": radio_name,
-                            "origin": server_info.get('origin'),
-                            "server_name": server_name
-                        },
-                        "time": current_time,
-                        "fields": {
-                            "value": current_song_r.content
-                        }
-                    })
-                break
+                    return 1 if shoutcast_info.group(1) else 0, int(shoutcast_info.group(2))
         except ConnectionError as e:
-            pass
+            print e
         except Exception as e:
             print e
         finally:
+            retries -= 1
             time.sleep(0.5)
+        return 0, 0
 
-print measurements
-# PUSH the stats
-client = InfluxDBClient('influxdb', 8086, 'shoutcast', '1shoutcast!', 'shoutcast')
-client.write_points(measurements) 
+def get_radio_stats(radio_id,  radio_info):
+    total, values, measurements = 0, {"total": 0}, []
+
+    if radio_info.get('servers', []):
+        current_radio_show_name = get_radio_show_name(radio_info.get('servers')[0])
+    for server in radio_info.get('servers', []):
+        origin = server.get('origin', 'web')
+        status, current_listeners = get_server_stats(server)
+        measurements.append({
+            "measurement": "radio_server_status",
+            "tags": {
+                "radio_name": radio_info.get('radio_name', radio_id),
+                "radio_show_name": current_radio_show_name,
+                "server_name": "{}:{}".format(server.get('host'), server.get('port')),
+                "origin": origin
+            },
+            "time": CURRENT_TIME,
+            "fields": {
+                "status": status
+            }
+        })
+
+        if origin not in values.keys():
+            values[origin] = 0
+        values[origin] += current_listeners
+
+    for key, value in values.iteritems():
+        if key != 'total':
+            values['total'] += value
+
+    return measurements + [
+        {
+            "measurement": "radio_listeners",
+            "tags": {
+                "radio_name": radio_info.get('radio_name', radio_id),
+                "radio_show_name": current_radio_show_name 
+            },
+            "time": CURRENT_TIME,
+            "fields": values
+        }
+    ]
+
+if __name__ == "__main__":
+    file_path = "/opt/monitor/radios.yaml"
+    if len(sys.argv) > 1:
+        file_path = sys.argv[1]
+
+    radios = load_radios(file_path)
+    measurements = []
+    for radio_id, radio_info in radios.iteritems():
+        measurements += get_radio_stats(radio_id, radio_info)
+
+    print json.dumps(measurements, indent=1)
+    # PUSH the stats
+    client = InfluxDBClient('influxdb', 8086, 'shoutcast', '1shoutcast!', 'shoutcast')
+    client.write_points(measurements) 
